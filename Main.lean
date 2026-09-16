0f21c312569872a23186fe6c6967b227a4443156
dxo <hi@d-xo.org>
2026-09-16 15:09:00 +0200

    init

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Shuffler.Lean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
--- flake.nix
+++ flake.nix
@@ -1,13 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
+    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs: {
-    packages = builtins.mapAttrs (system: pkgs: {
-      hello = pkgs.hello;
-
-      default = inputs.self.packages.${system}.hello;
-    }) inputs.nixpkgs.legacyPackages;
-  };
+  outputs = inputs:
+  inputs.flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import inputs.nixpkgs { inherit system; }; in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [ pkgs.elan ];
+      };
+    });
 }
