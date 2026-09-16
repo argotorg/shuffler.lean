96a89af9adbed6e066d71e85905feb3d61864432
dxo <hi@d-xo.org>
2026-09-16 14:36:44 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
+  };
+
+  outputs = inputs: {
+    packages = builtins.mapAttrs (system: pkgs: {
+      hello = pkgs.hello;
+
+      default = inputs.self.packages.${system}.hello;
+    }) inputs.nixpkgs.legacyPackages;
+  };
+}
